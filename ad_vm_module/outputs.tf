5920d8e435ff32bb6b5c275a9ed47e54ce4908a9
Stormy Winters <roameeo@outlook.com>
2023-08-16 07:17:56 -0500

    Error corrections
    More errors

--- ad_vm_module/outputs.tf
+++ ad_vm_module/outputs.tf
@@ -1,9 +1,16 @@
-output "ad_vms" {
-  value = azurerm_virtual_machine.ad_vms
+output "ad_vms_names" {
+  value       = azurerm_virtual_machine.ad_vms[*].name
+  description = "Names of the Active Directory VMs"
 }
 
-output "ad_nics" {
-  value = azurerm_network_interface.ad_nics
+output "ad_vms_ids" {
+  value       = azurerm_virtual_machine.ad_vms[*].id
+  description = "IDs of the Active Directory VMs"
+}
+
+output "ad_nics_ids" {
+  value       = azurerm_network_interface.ad_nics[*].id
+  description = "IDs of the Network Interfaces for Active Directory VMs"
 }
 
 output "admin_password" {
