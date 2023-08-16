3836449a325dfbbe300a68392b494fa5faaf2a89
Stormy Winters <roameeo@outlook.com>
2023-08-16 12:38:31 -0500

    Updated variables.tf
    Added all North Central RSG's and subnets

--- variables.tf
+++ variables.tf
@@ -8,14 +8,114 @@ variable "existing_resource_group_name2" {
   default = "SCUS-PRD-MGMT"
 }
 
+variable "existing_resource_group_name3" {
+  type    = string
+  default = "NCUS-DR-RSG"
+}
+
+variable "existing_resource_group_name4" {
+  type    = string
+  default = "NCUS-DR-MGMT"
+}
+
 variable "existing_virtual_network_name" {
   type    = string
-  default = "SC-VNET01-SC"
+  default = "SB-VNET01-SC"
+}
+
+variable "existing_virtual_network_name2" {
+  type    = string
+  default = "SB-VNET-02-NC"
 }
 
 variable "existing_subnet_name" {
   type = string
+  default = "DMZ-SUBNET-SC"
+}
+
+variable "existing_subnet_name11" {
+  type = string
+  default = "DMZ-SUBNET-NC"
+}
+
+variable "existing_subnet_name2" {
+  type = string
   default = "AD-SUBNET-SC"
+}
+
+variable "existing_subnet_name12" {
+  type = string
+  default = "AD-SUBNET-NC"
+}
+
+variable "existing_subnet_name3" {
+  type = string
+  default = "APPSERVERS-SUBNET-SC"
+}
+
+variable "existing_subnet_name13" {
+  type = string
+  default = "APPSERVERS-SUBNET-NC"
+}
+
+variable "existing_subnet_name4" {
+  type = string
+  default = "INFOSEC-SUBNET-SC"
+}
+
+variable "existing_subnet_name14" {
+  type = string
+  default = "INFOSEC-SUBNET-NC"
+}
+
+variable "existing_subnet_name5" {
+  type = string
+  default = "SQLSERVERS-SUBNET-SC"
+}
+
+variable "existing_subnet_name15" {
+  type = string
+  default = "SQLSERVERS-SUBNET-NC"
+}
+
+variable "existing_subnet_name6" {
+  type = string
+  default = "ITTOOLS-SUBNET-SC"
+}
+
+variable "existing_subnet_name16" {
+  type = string
+  default = "ITTOOLS-SUBNET-NC"
+}
+
+variable "existing_subnet_name7" {
+  type = string
+  default = "NETSERVERS-SUBNET-SC"
+}
+
+variable "existing_subnet_name17" {
+  type = string
+  default = "NETSERVERS-SUBNET-NC"
+}
+
+variable "existing_subnet_name8" {
+  type = string
+  default = "FILESERVERS-SUBNET-SC"
+}
+
+variable "existing_subnet_name18" {
+  type = string
+  default = "FILESERVERS-SUBNET-NC"
+}
+
+variable "existing_subnet_name9" {
+  type = string
+  default = "MOVEIT-SUBNET-SC"
+}
+
+variable "existing_subnet_name19" {
+  type = string
+  default = "MOVEIT-SUBNET-NC"
 }
 
 variable "location" {
