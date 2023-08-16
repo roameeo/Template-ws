465e0c47d9f4ab17f5381e0fa2f2ec86b9afde6f
Stormy Winters <roameeo@outlook.com>
2023-08-16 08:15:52 -0500

    Forgot a variable

--- ad_vm_module/variables.tf
+++ ad_vm_module/variables.tf
@@ -8,6 +8,11 @@ variable "existing_resource_group_name2" {
   description = "Name of the existing resource group for networking"
 }
 
+variable "existing_virtual_network_name" {
+  type    = string
+  default = "SC-VNET01-SC"
+}
+
 variable "existing_subnet_name" {
   type = string
   default = "AD-SUBNET-SC"
