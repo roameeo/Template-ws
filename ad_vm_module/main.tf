be44f93dca04b43239da14ef01df608aafb6430d
Stormy Winters <roameeo@outlook.com>
2023-08-17 14:33:17 -0500

    Update main.tf

--- ad_vm_module/main.tf
+++ ad_vm_module/main.tf
@@ -6,63 +6,29 @@ resource "azurerm_subnet" "existing_subnet" {
   address_prefixes = ["10.15.3.0/24"]  # Replace this with your desired subnet IP range
 }
 
-resource "azurerm_network_interface" "ad_nics" {
-  for_each           = toset(var.ad_nic_names)
-  name               = each.key
-  location           = var.location
-  resource_group_name = var.existing_resource_group_name2
-
-  ip_configuration {
-    name                          = "ipconfig-${each.key}"
-    subnet_id                     = azurerm_subnet.existing_subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_virtual_machine" "ad_vms" {
-  for_each            = toset(var.ad_vm_names)
-
-  name                = each.key
+resource "azurerm_virtual_machine" "existing_vm1" {
+  name                = "DC01"
   location            = var.location
   resource_group_name = var.existing_resource_group_name
-
-  network_interface_ids = [azurerm_network_interface.ad_nics[var.ad_vm_nic_map[each.key]].id]
-
+  network_interface_ids = "dc01-nic"
   vm_size               = "Standard_D4s_v3"
   delete_os_disk_on_termination = true
+}
 
-  storage_os_disk {
-    name                = "osdisk-${each.key}"
-    caching             = "ReadWrite"
-    create_option       = "FromImage"
-    managed_disk_type   = "Standard_LRS"
-  }
+resource "azurerm_virtual_machine" "existing_vm2" {
+  name                = "DC02"
+  location            = var.location
+  resource_group_name = var.existing_resource_group_name
+  network_interface_ids = "dc02-nic"
+  vm_size               = "Standard_D4s_v3"
+  delete_os_disk_on_termination = true
+}
 
-  storage_image_reference {
-    publisher           = "MicrosoftWindowsServer"
-    offer               = "WindowsServer"
-    sku                 = "2022-Datacenter-AzureEdition"
-    version             = "latest"
-  }
-
-  os_profile {
-    computer_name        = each.key
-    admin_username       = "azureadmin"
-    admin_password       = var.admin_password
-  }
-
-  os_profile_windows_config {
-    provision_vm_agent  = true
-  }
-
-  boot_diagnostics {
-    enabled             = true
-    storage_uri = "https://susserbankbootdiag.file.core.windows.net/"
-  }
-
-  tags = {
-    ServerType = "Active Directory"
-    buildby     = "Stormy Winters"
-    BuildDate   = "08/15/2023"
-  }
+resource "azurerm_virtual_machine" "existing_vm3" {
+  name                = "CA01"
+  location            = var.location
+  resource_group_name = var.existing_resource_group_name
+  network_interface_ids = "ca01-nic"
+  vm_size               = "Standard_D4s_v3"
+  delete_os_disk_on_termination = true
 }
