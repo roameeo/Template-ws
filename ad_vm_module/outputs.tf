f0895b276ae67bd90af8fbdeba1f9a8477a7f176
Stormy Winters <roameeo@outlook.com>
2023-08-17 14:35:48 -0500

    Update outputs.tf

--- ad_vm_module/outputs.tf
+++ ad_vm_module/outputs.tf
@@ -1,15 +1,14 @@
-output "ad_vm_names" {
-  value       = azurerm_virtual_machine.ad_vms
+output "ad_vm_names1" {
+  value       = azurerm_virtual_machine.existing_vm1
   description = "Names of the Active Directory VMs"
 }
 
-output "ad_nics" {
-  value       = azurerm_network_interface.ad_nics
+output "ad_vm_names2" {
+  value       = azurerm_virtual_machine.existing_vm2
   description = "Network Interfaces for Active Directory VMs"
 }
 
-output "admin_password" {
-  value       = var.admin_password
-  sensitive   = true
-  description = "Auto-generated admin password for the virtual machine"
+output "ad_vm_names3" {
+  value       = azurerm_virtual_machine.existing_vm3
+  description = "Network Interfaces for Active Directory VMs"
 }
