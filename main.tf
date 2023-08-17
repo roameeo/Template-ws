08246af2583bffb57cd073082e9e6396d3c5af22
Stormy Winters <roameeo@outlook.com>
2023-08-17 13:27:21 -0500

    Updated main.tf

--- main.tf
+++ main.tf
@@ -24,3 +24,10 @@ module "ad_vm_module" {
 #Admin password
   admin_password = random_password.admin_password.result
 }
+
+resource "azurerm_virtual_network" "existing_virtual_network_name2" {
+  name                = "SB-VNET-02-NC"
+  resource_group_name = var.existing_resource_group_name2
+  location            = var.location
+  address_space       = ["10.16.0.0/16"] 
+}
