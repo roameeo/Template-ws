75e2a8cf8eefd3782775b34b2b22426999a8dc8f
Stormy Winters <roameeo@outlook.com>
2023-08-22 12:57:26 -0500

    Update variables.tf

--- ad_vm_module/variables.tf
+++ ad_vm_module/variables.tf
@@ -1,36 +1,21 @@
-variable "existing_resource_group_name" {
-  type = string
-  description = "Name of the existing resource group for VMs"
-}
-
-variable "existing_resource_group_name2" {
-  type = string
-  description = "Name of the existing resource group for networking"
-}
-
-variable "existing_virtual_network_name" {
+variable "existing_resource_group_name_from_state" {
   type    = string
-  default = "SC-VNET01-SC"
+  default = null
 }
 
-variable "existing_subnet_name" {
-  type = string
-  default = "AD-SUBNET-SC"
+variable "existing_resource_group_name2_from_state" {
+  type    = string
+  default = null
 }
 
-variable "existing_network_interface" {
-  type = string
-  default = "dc01-nic"
+variable "existing_virtual_network_name_from_state" {
+  type    = string
+  default = null
 }
 
-variable "existing_network_interface2" {
-  type = string
-  default = "dc02-nic"
-}
-
-variable "exisiting_network_interface3"{
-  type = string
-  default = "ca01-nic"
+variable "existing_subnet_name_from_state" {
+  type    = string
+  default = null
 }
 
 variable "existing_vm_names" {
